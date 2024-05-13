8f9cfa95bcdf918c94320e880610a4be9af0dc0d
Pogorelyisp <pogorelyisp@yandex.ru>
2024-05-13 19:32:59 +0300

    Added an extended test
    Added an extended test that checks the maximum value not at the beginning of the array

--- src/test/java/StatisticsServiceTest.java
+++ src/test/java/StatisticsServiceTest.java
@@ -16,5 +16,21 @@ public class StatisticsServiceTest {
         long actual = service.findMax(incomesInBillions);
 
         Assertions.assertEquals(expected, actual);
+
     }
+    @Test
+
+    //  в этом тесте максимальное число ставим не в первую ячейку массива
+
+    void findMaxAdvanced() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {1, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 12;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected,actual);
+    }
+
 }
